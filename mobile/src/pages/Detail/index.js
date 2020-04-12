7fa508eb5a1a0e57b8f933eeba63a33141913f12
Ricardo Balbi <rbalbi@gmail.com>
2020-04-11 21:05:13 -0300

    feat(mobile): add detail layout

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,14 +1,264 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 
-export default function Detail({ route }) {
-  const { category } = route.params;
+import { FontAwesome } from '@expo/vector-icons';
 
-  console.log(category.category);
+import {
+  Container,
+  Category,
+  CategoryTitle,
+  HeaderTable,
+  HeaderTableText,
+  TeamView,
+  Team,
+  PositionText,
+  TeamShield,
+  TeamText,
+  Score,
+  ScoreText,
+  RoundView,
+  RoundText,
+  DateView,
+  DateText,
+  MatchView,
+  MatchTeamText,
+  MatchTeamShield,
+} from './styles';
 
+import sampImg from '../../assets/sampdoria.png';
+import juvImg from '../../assets/juventus.png';
+import interImg from '../../assets/inter-de-milao.png';
+import milanImg from '../../assets/milan.png';
+
+export default function Main() {
   return (
-    <View>
-      <Text>Cat: {category.category}</Text>
-    </View>
+    <Container>
+      <Category>
+        <CategoryTitle>TORNEIO A - TABELA</CategoryTitle>
+
+        <HeaderTable>
+          <HeaderTableText>P</HeaderTableText>
+          <HeaderTableText>J</HeaderTableText>
+          <HeaderTableText>V</HeaderTableText>
+          <HeaderTableText>E</HeaderTableText>
+          <HeaderTableText>D</HeaderTableText>
+          <HeaderTableText>SG</HeaderTableText>
+        </HeaderTable>
+
+        <TeamView>
+          <Team>
+            <PositionText>1</PositionText>
+            <TeamShield source={sampImg} />
+            <MatchTeamText>SAM</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>1</ScoreText>
+            <ScoreText>2</ScoreText>
+            <ScoreText>3</ScoreText>
+            <ScoreText>4</ScoreText>
+            <ScoreText>5</ScoreText>
+            <ScoreText>6</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>2</PositionText>
+            <TeamShield source={juvImg} />
+            <MatchTeamText>JUV</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>7</ScoreText>
+            <ScoreText>8</ScoreText>
+            <ScoreText>9</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>11</ScoreText>
+            <ScoreText>12</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>3</PositionText>
+            <TeamShield source={interImg} />
+            <MatchTeamText>INT</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>13</ScoreText>
+            <ScoreText>14</ScoreText>
+            <ScoreText>15</ScoreText>
+            <ScoreText>16</ScoreText>
+            <ScoreText>17</ScoreText>
+            <ScoreText>18</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>4</PositionText>
+            <TeamShield source={milanImg} />
+            <MatchTeamText>MIL</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>19</ScoreText>
+            <ScoreText>20</ScoreText>
+            <ScoreText>21</ScoreText>
+            <ScoreText>22</ScoreText>
+            <ScoreText>23</ScoreText>
+            <ScoreText>24</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>5</PositionText>
+            <TeamShield source={sampImg} />
+            <MatchTeamText>SAM</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>25</ScoreText>
+            <ScoreText>26</ScoreText>
+            <ScoreText>27</ScoreText>
+            <ScoreText>28</ScoreText>
+            <ScoreText>29</ScoreText>
+            <ScoreText>30</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>6</PositionText>
+            <TeamShield source={juvImg} />
+            <MatchTeamText>JUV</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>30</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>20</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>7</PositionText>
+            <TeamShield source={interImg} />
+            <MatchTeamText>INT</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>30</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>20</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>8</PositionText>
+            <TeamShield source={milanImg} />
+            <MatchTeamText>MIL</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>30</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>20</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>9</PositionText>
+            <TeamShield source={interImg} />
+            <MatchTeamText>INT</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>30</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>20</ScoreText>
+          </Score>
+        </TeamView>
+        <TeamView>
+          <Team>
+            <PositionText>10</PositionText>
+            <TeamShield source={milanImg} />
+            <MatchTeamText>MIL</MatchTeamText>
+          </Team>
+          <Score>
+            <ScoreText>30</ScoreText>
+            <ScoreText>10</ScoreText>
+            <ScoreText>20</ScoreText>
+          </Score>
+        </TeamView>
+      </Category>
+
+      <Category>
+        <CategoryTitle>JOGOS</CategoryTitle>
+        <RoundView>
+          <TouchableOpacity onPress={() => {}}>
+            <FontAwesome name='chevron-left' size={16} color='#1e7a0e' />
+          </TouchableOpacity>
+          <RoundText>1ª RODADA</RoundText>
+          <TouchableOpacity onPress={() => {}}>
+            <FontAwesome name='chevron-right' size={16} color='#1e7a0e' />
+          </TouchableOpacity>
+        </RoundView>
+
+        <DateView>
+          <DateText>20/10</DateText>
+        </DateView>
+        <MatchView>
+          <MatchTeamText>SAM</MatchTeamText>
+          <MatchTeamShield source={sampImg}></MatchTeamShield>
+          <MatchTeamText>2</MatchTeamText>
+          <MatchTeamText>X</MatchTeamText>
+          <MatchTeamText>0</MatchTeamText>
+          <MatchTeamShield source={milanImg}></MatchTeamShield>
+          <MatchTeamText>MIL</MatchTeamText>
+        </MatchView>
+
+        <DateView>
+          <DateText>20/10</DateText>
+        </DateView>
+        <MatchView>
+          <MatchTeamText>SAM</MatchTeamText>
+          <MatchTeamShield source={sampImg}></MatchTeamShield>
+          <MatchTeamText>2</MatchTeamText>
+          <MatchTeamText>X</MatchTeamText>
+          <MatchTeamText>0</MatchTeamText>
+          <MatchTeamShield source={milanImg}></MatchTeamShield>
+          <MatchTeamText>MIL</MatchTeamText>
+        </MatchView>
+
+        <DateView>
+          <DateText>20/10</DateText>
+        </DateView>
+        <MatchView>
+          <MatchTeamText>SAM</MatchTeamText>
+          <MatchTeamShield source={sampImg}></MatchTeamShield>
+          <MatchTeamText>2</MatchTeamText>
+          <MatchTeamText>X</MatchTeamText>
+          <MatchTeamText>0</MatchTeamText>
+          <MatchTeamShield source={milanImg}></MatchTeamShield>
+          <MatchTeamText>MIL</MatchTeamText>
+        </MatchView>
+
+        <DateView>
+          <DateText>20/10</DateText>
+        </DateView>
+        <MatchView>
+          <MatchTeamText>SAM</MatchTeamText>
+          <MatchTeamShield source={sampImg}></MatchTeamShield>
+          <MatchTeamText>2</MatchTeamText>
+          <MatchTeamText>X</MatchTeamText>
+          <MatchTeamText>0</MatchTeamText>
+          <MatchTeamShield source={milanImg}></MatchTeamShield>
+          <MatchTeamText>MIL</MatchTeamText>
+        </MatchView>
+
+        <DateView>
+          <DateText>20/10</DateText>
+        </DateView>
+        <MatchView>
+          <MatchTeamText>SAM</MatchTeamText>
+          <MatchTeamShield source={sampImg}></MatchTeamShield>
+          <MatchTeamText>2</MatchTeamText>
+          <MatchTeamText>X</MatchTeamText>
+          <MatchTeamText>0</MatchTeamText>
+          <MatchTeamShield source={milanImg}></MatchTeamShield>
+          <MatchTeamText>MIL</MatchTeamText>
+        </MatchView>
+      </Category>
+    </Container>
   );
 }
---
+++ mobile/src/pages/Detail/styles.js
@@ -0,0 +1,141 @@
+import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: #f0f0f0;
+  padding: ${Constants.statusBarHeight + 5}px 14px 0px;
+`;
+
+export const Category = styled.View`
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 8px;
+  margin-bottom: 16px;
+`;
+
+export const CategoryTitle = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #1e7a0e;
+  margin-bottom: 5px;
+`;
+
+export const HeaderTable = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  padding-bottom: 5px;
+  border-bottom-width: 1px;
+  border-bottom-color: #e0e0e0;
+`;
+
+export const HeaderTableText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #999;
+  margin-left: 15px;
+  min-width: 20px;
+
+  justify-content: center;
+`;
+
+export const TeamView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+  padding-bottom: 5px;
+  border-bottom-width: 1px;
+  border-bottom-color: #e0e0e0;
+`;
+
+export const Team = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const PositionText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #999;
+  padding-right: 5px;
+  min-width: 20px;
+`;
+
+export const TeamShield = styled.Image`
+  width: 20px;
+  height: 20px;
+`;
+
+export const TeamText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #717171;
+  padding-left: 10px;
+`;
+
+export const Score = styled.View`
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const ScoreText = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #999;
+  margin-left: 15px;
+  min-width: 20px;
+`;
+
+export const RoundView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+
+  border-top-width: 1px;
+  border-top-color: #e0e0e0;
+  border-bottom-width: 1px;
+  border-bottom-color: #e0e0e0;
+`;
+
+export const RoundText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: #999;
+`;
+
+export const DateView = styled.View`
+  align-items: center;
+`;
+
+export const DateText = styled.Text`
+  color: #999;
+`;
+
+export const MatchView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  border-bottom-width: 1px;
+  border-bottom-color: #e0e0e0;
+
+  margin-bottom: 10px;
+`;
+
+export const MatchTeamText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #717171;
+  padding-left: 10px;
+`;
+
+export const MatchTeamShield = styled.Image`
+  width: 25px;
+  height: 25px;
+`;
