10a39a90a1742ed0265a38ec51109634a22b4fca
Ricardo Balbi <rbalbi@gmail.com>
2020-04-13 16:39:39 -0300

    chore(config): add jest and lint-staged to all sub-repos
    This action aim to verify if the code do not broke down

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  // Stop running tests after `n` failures
+  bail: true,
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: false,
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: ['src/**', '!src/database/migrations/**'],
+  // The directory where Jest should output its coverage files
+  // coverageDirectory: 'coverage',
+  // The test environment that will be used for testing
+  testEnvironment: "node",
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.test.js?(x)",
+    // "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+};
