4dd0e7ce28108d2d4d5d109749505b3b7cf3f0ef
Ricardo Balbi <rbalbi@gmail.com>
2020-05-12 18:43:05 -0300

    feat(mobile): updateMatch - initial styled configuration

--- frontend/src/pages/UpdateMatches/index.js
+++ frontend/src/pages/UpdateMatches/index.js
@@ -1,5 +1,63 @@
 import React from 'react';
 
+import {
+  Container,
+  CategoryTitle,
+  CategoryResult,
+  ClassificationContainer,
+  ClassificationTitle,
+  HeaderTable,
+  HeaderTableText,
+  TeamView,
+  Team,
+  TeamText,
+  PositionText,
+  TeamShield,
+  Score,
+  ScoreText,
+  MatchContainer,
+} from './styles';
+
 export default function UpdateMatches() {
-  return <h1>Update Matches !!</h1>;
+  return (
+    <Container>
+      <CategoryTitle>TORNEIO A</CategoryTitle>
+      <CategoryResult>
+        <ClassificationContainer>
+          <ClassificationTitle>CLASSIFICAÇÃO</ClassificationTitle>
+          <HeaderTable>
+            <HeaderTableText>P</HeaderTableText>
+            <HeaderTableText>J</HeaderTableText>
+            <HeaderTableText>V</HeaderTableText>
+            <HeaderTableText>E</HeaderTableText>
+            <HeaderTableText>D</HeaderTableText>
+            <HeaderTableText>GP</HeaderTableText>
+            <HeaderTableText>GC</HeaderTableText>
+            <HeaderTableText>SG</HeaderTableText>
+            <HeaderTableText>%</HeaderTableText>
+          </HeaderTable>
+
+          <TeamView>
+            <Team>
+              <PositionText>1</PositionText>
+              <TeamShield src="https://rb-calcio.herokuapp.com/files/shields/sampdoria.png" />
+              <TeamText>SAMPDORIA</TeamText>
+            </Team>
+            <Score>
+              <ScoreText>12</ScoreText>
+              <ScoreText score>4</ScoreText>
+              <ScoreText score>6</ScoreText>
+              <ScoreText score>87</ScoreText>
+              <ScoreText score>34</ScoreText>
+              <ScoreText score>88</ScoreText>
+              <ScoreText score>5</ScoreText>
+              <ScoreText score>6</ScoreText>
+              <ScoreText score>100</ScoreText>
+            </Score>
+          </TeamView>
+        </ClassificationContainer>
+        <MatchContainer></MatchContainer>
+      </CategoryResult>
+    </Container>
+  );
 }
---
+++ frontend/src/pages/UpdateMatches/styles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #fff;
+  padding: 20px;
+  margin: 20px;
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const CategoryTitle = styled.h1``;
+
+export const CategoryResult = styled.div``;
+
+export const ClassificationContainer = styled.div``;
+
+export const ClassificationTitle = styled.h2``;
+
+export const HeaderTable = styled.div``;
+
+export const HeaderTableText = styled.h3``;
+
+export const TeamView = styled.div``;
+
+export const Team = styled.div``;
+
+export const TeamText = styled.h3``;
+
+export const PositionText = styled.h3``;
+
+export const TeamShield = styled.img``;
+
+export const Score = styled.div``;
+
+export const ScoreText = styled.h3``;
+
+export const MatchContainer = styled.div``;
