7e09545fa9916fdcd1cf49975f7d1898449959dc
Ricardo Balbi <rbalbi@gmail.com>
2020-07-03 14:36:13 -0300

    feat(mobile): add suspense and lazy components to route

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,58 +1,61 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import { Text } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Main from './pages/Main';
-import Detail from './pages/Detail';
-import Rule from './pages/Rule';
-import RuleIcon from './components/RuleIcon';
+const Main = lazy(() => import('./pages/Main'));
+const Detail = lazy(() => import('./pages/Detail'));
+const Rule = lazy(() => import('./pages/Rule'));
+const RuleIcon = lazy(() => import('./components/RuleIcon'));
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          title: 'Calcio',
-          headerStyle: { backgroundColor: '#1E7A0E' },
-          headerTintColor: '#FFF',
-          headerTitleStyle: {
-            fontFamily: 'Ubuntu_700Bold',
-            fontWeight: 'bold',
-            fontSize: 24,
-          },
-          headerTitleAlign: 'center',
-        }}
-      >
-        <Stack.Screen
-          name="Calcio"
-          component={Main}
-          options={{
+      <Suspense fallback={<Text>Carregando...</Text>}>
+        <Stack.Navigator
+          screenOptions={{
             title: 'Calcio',
-            headerRight: () => <RuleIcon />,
+            headerStyle: { backgroundColor: '#1E7A0E' },
+            headerTintColor: '#FFF',
+            headerTitleStyle: {
+              fontFamily: 'Ubuntu_700Bold',
+              fontWeight: 'bold',
+              fontSize: 24,
+            },
+            headerTitleAlign: 'center',
           }}
-        />
+        >
+          <Stack.Screen
+            name="Calcio"
+            component={Main}
+            options={{
+              title: 'Calcio',
+              headerRight: () => <RuleIcon />,
+            }}
+          />
 
-        <Stack.Screen
-          name="Category"
-          component={Detail}
-          options={({ route }) => ({
-            title: `TORNEIO ${route.params.info.category}`,
-            headerBackTitleVisible: false,
-          })}
-        />
+          <Stack.Screen
+            name="Category"
+            component={Detail}
+            options={({ route }) => ({
+              title: `TORNEIO ${route.params.info.category}`,
+              headerBackTitleVisible: false,
+            })}
+          />
 
-        <Stack.Screen
-          name="Rule"
-          component={Rule}
-          options={{
-            title: 'Regulamento',
-            headerBackTitleVisible: false,
-          }}
-        />
-      </Stack.Navigator>
+          <Stack.Screen
+            name="Rule"
+            component={Rule}
+            options={{
+              title: 'Regulamento',
+              headerBackTitleVisible: false,
+            }}
+          />
+        </Stack.Navigator>
+      </Suspense>
     </NavigationContainer>
   );
 }
