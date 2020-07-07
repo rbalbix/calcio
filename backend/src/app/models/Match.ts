4c9898365543b0e932cb41fb8c3a5bd854216110
Ricardo Balbi <rbalbi@gmail.com>
2020-07-07 15:57:41 -0300

    refactor(backend): error handling

--- backend/src/app/models/Match.ts
+++ backend/src/app/models/Match.ts
@@ -74,70 +74,82 @@ const MatchSchema = new Schema(
 
 // Trigger to update Rank
 MatchSchema.post('findOneAndUpdate', async function (doc: IMatch) {
-  await updateRank(doc);
+  try {
+    await updateRank(doc);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 MatchSchema.post('save', async function (doc: IMatch) {
-  await updateRank(doc);
+  try {
+    await updateRank(doc);
+  } catch (err) {
+    throw new Error(err);
+  }
 });
 
 export const Match = model<IMatch>('Match', MatchSchema);
 
 async function updateRank(doc: IMatch) {
-  if (doc.round <= 27 && doc.scoreHome !== null && doc.scoreAway !== null) {
-    const drawn = doc.scoreHome === doc.scoreAway;
-    const homeWon = doc.scoreHome > doc.scoreAway;
+  try {
+    if (doc.round <= 27 && doc.scoreHome !== null && doc.scoreAway !== null) {
+      const drawn = doc.scoreHome === doc.scoreAway;
+      const homeWon = doc.scoreHome > doc.scoreAway;
 
-    await Rank.findOneAndUpdate(
-      {
-        champ: doc.champ,
-        category: doc.category,
-        team: doc.teamHome,
-      },
-      {
-        champ: doc.champ,
-        category: doc.category,
-        team: doc.teamHome,
-        $inc: {
-          points: drawn ? 1 : homeWon ? 3 : 0,
-          played: 1,
-          wons: homeWon ? 1 : 0,
-          drawn: drawn ? 1 : 0,
-          lost: drawn ? 0 : homeWon ? 0 : 1,
-          goalsFor: doc.scoreHome,
-          goalsAgainst: doc.scoreAway,
-          goalDifference: doc.scoreHome - doc.scoreAway,
+      await Rank.findOneAndUpdate(
+        {
+          champ: doc.champ,
+          category: doc.category,
+          team: doc.teamHome,
         },
-      },
-      {
-        upsert: true,
-      }
-    );
+        {
+          champ: doc.champ,
+          category: doc.category,
+          team: doc.teamHome,
+          $inc: {
+            points: drawn ? 1 : homeWon ? 3 : 0,
+            played: 1,
+            wons: homeWon ? 1 : 0,
+            drawn: drawn ? 1 : 0,
+            lost: drawn ? 0 : homeWon ? 0 : 1,
+            goalsFor: doc.scoreHome,
+            goalsAgainst: doc.scoreAway,
+            goalDifference: doc.scoreHome - doc.scoreAway,
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
 
-    await Rank.findOneAndUpdate(
-      {
-        champ: doc.champ,
-        category: doc.category,
-        team: doc.teamAway,
-      },
-      {
-        champ: doc.champ,
-        category: doc.category,
-        team: doc.teamAway,
-        $inc: {
-          points: drawn ? 1 : !homeWon ? 3 : 0,
-          played: 1,
-          wons: drawn ? 0 : !homeWon ? 1 : 0,
-          drawn: drawn ? 1 : 0,
-          lost: drawn ? 0 : homeWon ? 1 : 0,
-          goalsFor: doc.scoreAway,
-          goalsAgainst: doc.scoreHome,
-          goalDifference: doc.scoreAway - doc.scoreHome,
+      await Rank.findOneAndUpdate(
+        {
+          champ: doc.champ,
+          category: doc.category,
+          team: doc.teamAway,
         },
-      },
-      {
-        upsert: true,
-      }
-    );
+        {
+          champ: doc.champ,
+          category: doc.category,
+          team: doc.teamAway,
+          $inc: {
+            points: drawn ? 1 : !homeWon ? 3 : 0,
+            played: 1,
+            wons: drawn ? 0 : !homeWon ? 1 : 0,
+            drawn: drawn ? 1 : 0,
+            lost: drawn ? 0 : homeWon ? 1 : 0,
+            goalsFor: doc.scoreAway,
+            goalsAgainst: doc.scoreHome,
+            goalDifference: doc.scoreAway - doc.scoreHome,
+          },
+        },
+        {
+          upsert: true,
+        }
+      );
+    }
+  } catch (err) {
+    throw new Error(err);
   }
 }
