593b6a45d2e14c8c8f029f1f89f29fa3b3b3c0b2
Ricardo Balbi <rbalbi@gmail.com>
2020-07-22 17:02:03 -0300

    fix(config): url address

--- mobile/src/config/index.js
+++ mobile/src/config/index.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://192.168.0.5:3333';
-// export const baseURL = 'https://rb-calcio.herokuapp.com';
+// export const baseURL = 'http://192.168.0.5:3333';
+export const baseURL = 'https://rb-calcio.herokuapp.com';
